a106ee4822062014eb0fd4b841956b826f1935f0
supertrack49 <115777615+supertrack49@users.noreply.github.com>
2026-02-10 10:55:23 +0000

    codice aggiornato con domanda

--- llm.py
+++ llm.py
@@ -1,6 +1,19 @@
 from dotenv import dotenv_values
+from langchain.chat_models import init_chat_model
 
 config=dotenv_values(".env")
 
 print(config)
 print(config["MODEL"])
+
+llm = init_chat_model(config['MODEL']
+        , temperature= config['TEMPERATURE']
+        , api_key= config['key']
+        , verbose= True
+        , base_url= "https://api.cerebras.ai/v1/") #indirizzo del server di cerebras 
+
+risposta = llm.invoke("chi è Mira Murati? rispondi brevemente")
+
+print(type(risposta))
+
+print(risposta.content)
