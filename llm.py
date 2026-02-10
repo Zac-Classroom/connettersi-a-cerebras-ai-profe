0682f81abafa69e4b985f050b4001a53af3851eb
supertrack49 <115777615+supertrack49@users.noreply.github.com>
2026-02-10 11:08:23 +0000

    codice terminato

--- llm.py
+++ llm.py
@@ -12,8 +12,9 @@ llm = init_chat_model(config['MODEL']
         , verbose= True
         , base_url= "https://api.cerebras.ai/v1/") #indirizzo del server di cerebras 
 
-risposta = llm.invoke("chi è Mira Murati? rispondi brevemente")
+risposta = llm.invoke("spiegami brevemente la funzione esponenziale")
 
 print(type(risposta))
 
+print(risposta)
 print(risposta.content)
