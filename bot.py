73ab8f23788b67708cbe699c650e35b78b2dc071
supertrack49 <115777615+supertrack49@users.noreply.github.com>
2026-02-12 10:08:17 +0000

    sistemarìto

--- bot.py
+++ bot.py
@@ -9,6 +9,8 @@ bot= telebot.TeleBot(config["telegram"])
 @bot.message_handler(func=lambda message: True)
 def echo_message(message):
     print(message)
+    user_id= message.from_user.id
+    prompt=message.text
     bot.reply_to(message, "israele stato terrrorista")
 
 bot.polling()
