f672079c808226040ba90c36afbdbb88f23f52cb
supertrack49 <115777615+supertrack49@users.noreply.github.com>
2026-02-12 09:25:19 +0000

    bot.py

--- bot.py
+++ bot.py
@@ -1,15 +1,12 @@
 from dotenv import dotenv_values
 import telebot
 
-config = dotenv_values(".env")
+config=dotenv_values(".env")
+print(config)
 
-print (config)
+bot= telebot.Telebot(config["telegram"])
 
-bot = telebot.TeleBot(config["telegram"])
-
-@bot.message_handler(func = lambda message: True)
-def echo__message(message):
-    print (message)
-    bot.reply_to(message, "Si sono il miglior Dj della Puglia")
-
-bot.polling()
+@bot.message_handler(func=lambda message: True)
+def echo_message(message):
+    print(message)
+    
